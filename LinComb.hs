cb2d019c3525fb61fc7470724fdc6be8006c43c3
Ilya Yanok <ilya.yanok@gmail.com>
2010-12-17 21:04:48 +0300

    lincomb: simple monad to handle linear combinations

---
+++ LinComb.hs
@@ -0,0 +1,45 @@
+module LinComb where
+
+import Data.Map (toList,fromListWith)
+infixl 7 .*
+
+data LinComb b a = LC {runLC :: [(a,b)]} deriving (Show,Eq)
+
+mapC f (LC l) = LC $ map (\(e,c) -> (e,f c)) l
+
+instance Functor (LinComb b) where
+  fmap f (LC l) = LC $ map (\(e,c) -> (f e,c)) l
+
+a .* b = mapC (a*) b
+
+instance Num b => Monad (LinComb b) where
+  return x = LC [(x,1)]
+  l >>= f = LC $ concatMap (\(e,c) -> runLC $ c .* (f e)) (runLC l)
+
+instance (Eq a,Show a,Num b) => Num (LinComb b a) where
+  LC a + LC b = LC $ (a ++ b)
+  a - b = a + (-1) .* b
+  _ * _ = error "Num is annoying"
+  abs _ = error "Num is annoying"
+  signum _ = error "Num is annoying"
+  fromInteger a = if a==0 then LC [] else error "fromInteger can only take zero argument"
+
+collect :: (Num b,Ord a) => LinComb b a -> LinComb b a
+collect = LC . toList . fromListWith (+) . runLC
+  
+-- map function a -> LinComb a to every element in
+-- the multiplication (presented as list) at the same
+-- time
+mapAll :: Num b => (a -> LinComb b a) -> [a] -> LinComb b [a]
+mapAll f l = mapM f l
+
+-- map function to every element in the multiplication
+-- using the chain rule (for diff-like functions)
+mapChain :: (Eq a,Show a,Num b) => (a -> LinComb b a) -> [a] -> LinComb b [a]
+mapChain f l = sum $ map (\(i,x) -> mapM (actOn i) x) ls where
+  actOn i (n,x) | i == n = f x
+                | otherwise = return x
+  ls = zip [1..n] $ repeat lWithNmbrs
+  lWithNmbrs = zip [1..] l
+  n = length l
+
