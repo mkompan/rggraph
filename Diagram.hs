c5220a1d17bc9601e5d33ab14f795e4f176e3907
Ilya Yanok <ilya.yanok@gmail.com>
2010-12-20 15:35:29 +0300

    diagram: top-level diagram handling
    Now we support only putting 'dots' (partial derivatives by mu^2) on
    edges (via graphMap and thus broken)

---
+++ Diagram.hs
@@ -0,0 +1,16 @@
+module Diagram where
+
+import Graph hiding (dMuSquare)
+import LinComb
+import Theory
+
+data DExp = Diagram Diagram
+          | DElement DElement deriving (Show)
+            
+getDElement (DElement e) = e
+actOnElement f = (fmap getDElement) . f . DElement
+
+dMu2 :: DExp -> LinComb Rational DExp
+dMu2 (Diagram d) = fmap Diagram $ graphMapChain (actOnElement dMu2) d
+dMu2 (DElement ((DVertex,_),_)) = LC [] -- FIXME! I'd like to use 0 or mzero here
+dMu2 (DElement (p@(DProp,_),mods)) = return $ DElement (p,"dMu2":mods)
