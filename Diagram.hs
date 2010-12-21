92c82162caff01b7de41aa25929dc1d03e8b86b8
Ilya Yanok <ilya.yanok@gmail.com>
2010-12-21 04:37:35 +0300

    diagram: dP operation to put differentiation by external moment on elements

--- Diagram.hs
+++ Diagram.hs
@@ -3,6 +3,7 @@ module Diagram where
 import Graph hiding (dMuSquare)
 import LinComb
 import Theory
+import Moment
 
 data DExp = Diagram Diagram
           | DElement DElement deriving (Show)
@@ -14,3 +15,8 @@ dMu2 :: DExp -> LinComb Rational DExp
 dMu2 (Diagram d) = fmap Diagram $ graphMap mapChain (actOnElement dMu2) d
 dMu2 (DElement ((DVertex,_),_)) = LC [] -- FIXME! I'd like to use 0 or mzero here
 dMu2 (DElement (p@(DProp,_),mods)) = return $ DElement (p,"dMu2":mods)
+
+dP :: DExp -> LinComb Rational DExp
+dP (Diagram d) = fmap Diagram $ graphMap mapChain (actOnElement dP) d
+dP (DElement ((t,m),mods)) | m `hasMoment` 0 = return $ DElement ((t,m),"dP":mods)
+                           | otherwise = LC []
--- Moment.hs
+++ Moment.hs
@@ -47,6 +47,9 @@ instance Show Moment where
   
 addZeroMults (M' m') = M $ map (\(s,c) -> (s,c,[])) m'
 
+hasMoment (M m) n = n `elem` ns where
+  (_,ns,_) = unzip3 m
+
 -- stretch the moment by a_n factor along given list of basic moments
 momentStretch (M m) n l = M $ map stretchOne m where
   stretchOne (s,q,a) | q `elem` l = (s,q,n:a)
