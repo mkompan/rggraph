6134a016f27bef645e1dbead8b692dcf6906d4fd
Ilya Yanok <ilya.yanok@gmail.com>
2011-01-13 03:17:34 +0300

    angles: fix 1-loop jacobian printing

--- Angles.hs
+++ Angles.hs
@@ -94,5 +94,6 @@ jacobian n pairs = zip [1..] ls where
   clsdLayers = closeLayers order $ layerPairs order pairs
   ((_,order,_),_) = optimalOrder n pairs
 
+stringifyJ [] = "r**(d-1)"
 stringifyJ as = "r**(d-1)*" ++ (intercalate "*" $ map strOne as) where
   strOne (a,n) = "(sin(theta_" ++ show a ++ ")**(d-" ++ show (n+2) ++ "))"
